b1e7d7ca2ffd476f1b36fd73395eea3f9a684d4d
DragonsBellow <DragonsBellow@users.noreply.github.com>
2014-05-06 22:55:40 -0400

    Delete test.rb

--- test/test.rb
+++
@@ -1,9 +0,0 @@
-require '../lib/FanFictionDownloader'
-
-
-#fic = FanFictionDownloader::FIMNetGetter.new 'http://www.fimfiction.net/story/104188/valkyrie'
-
-
-
-test = FanFictionDownloader::FFNetGetter.new
-test.test
